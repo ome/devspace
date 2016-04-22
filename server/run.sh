1409ca17f68f51696d429551a11c119143177ad8
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-04-22 14:01:21 +0100

    remove custom omero-install branch and clean up job

--- server/run.sh
+++ server/run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 /tmp/jenkins-slave.sh
+
+source /tmp/omero-install/settings.env
+sudo chown omero "$OMERO_DATA_DIR"
+
 /home/omero/OMERO.server/bin/omero admin start
--- slave/run.sh
+++ slave/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 /tmp/jenkins-slave.sh
+
 /home/omero/workspace/OMERO-test-integration/src/dist/bin/omero admin start --foreground
