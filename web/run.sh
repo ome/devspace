f0943b887dc0cd105c723a348139beb413b9f811
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-04-26 19:43:35 +0100

    fix web startup

--- web/run.sh
+++ web/run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-source /home/omero/workspace/OMERO-web/omero-virtualenv/bin/activate; /home/omero/workspace/OMERO-web/OMERO.web/bin/omero web start --foreground &
+rm -rf /home/omero/workspace/OMERO-web/OMERO.web/django.pid
+source /home/omero/workspace/OMERO-web/omero-virtualenv/bin/activate; /home/omero/workspace/OMERO-web/OMERO.web/bin/omero web start &
 /tmp/jenkins-slave.sh
-
