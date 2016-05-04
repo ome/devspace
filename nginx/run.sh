df629c8c7b905d19a4f88a79a90c2975d476050e
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-05-04 11:20:47 +0100

    make sure nginx start in foreground

--- nginx/run.sh
+++ nginx/run.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 sudo chmod 777 -R /home/omero/static/
 
-function shut_down() {
-    sudo nginx -s stop
-}
-
 /tmp/jenkins-slave.sh &
-sudo nginx &
-
-trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
-
-wait
+sudo nginx -g "daemon off;"
