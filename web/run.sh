ce93c66a0ac198cf715b889e23152ef1c310201f
Ola Tarkowska <A.Tarkowska@dundee.ac.uk>
2016-05-04 11:48:59 +0100

    run web in foreground

--- web/run.sh
+++ web/run.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-djangopid=/home/omero/workspace/OMERO-web/OMERO.web/var/django.pid
+workspace=/home/omero/workspace/OMERO-web
+djangopid="$workspace/OMERO.web/var/django.pid"
 if [ -f $djangopid ]; then
     rm -f $djangopid
 fi
 
-function shut_down() {
-    source /home/omero/workspace/OMERO-web/omero-virtualenv/bin/activate; /home/omero/workspace/OMERO-web/OMERO.web/bin/omero web stop
-}
+if [ -d "$workspace" ]; then
+    source $workspace/omero-virtualenv/bin/activate; $workspace/OMERO.web/bin/omero web start --foregroud &
+fi
 
-/tmp/jenkins-slave.sh &
-source /home/omero/workspace/OMERO-web/omero-virtualenv/bin/activate; /home/omero/workspace/OMERO-web/OMERO.web/bin/omero web start
-
-trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
-
-wait
+/tmp/jenkins-slave.sh
