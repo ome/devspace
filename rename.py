260089d129e0f7741edc83d40443cdfc4347fd85
Jean-Marie Burel <j.burel@dundee.ac.uk>
2017-10-24 09:13:28 +0100

    Simplify replacement

--- rename.py
+++ rename.py
@@ -10,7 +10,6 @@ EXCLUDE = ["builds", "workspace", "fingerprints"]
 
 def replace(name, branch, uid, user):
   cnt = 0
-  cnt_user = 0
   for root, dirs, files in os.walk("."):
     dirs[:] = list(filter(lambda x: not x in EXCLUDE, dirs))
     yml = list(fnmatch.filter(files, "*.yml"))
@@ -18,19 +17,16 @@ def replace(name, branch, uid, user):
     sh = list(fnmatch.filter(files, "sslcert.sh"))
     for f in yml + xml + sh:
       fname = os.path.join(root, f)
-      print "Setting space to '%s' in %s" % (name, fname)
+      print "Setting space to '%s', %s' in %s" % (name, user, fname)
       for line in fileinput.input([fname], inplace=True):
-        regexp = re.compile(r'SPACE[NAME|BRANCH]')
-        regexp_user = re.compile(r'SPACEUSER')
+        regexp = re.compile(r'SPACE[NAME|BRANCH|USER]')
         if regexp.search(line) is not None:
           cnt += 1
           line = line.replace("SPACENAME", name)
           line = line.replace("SPACEBRANCH", name)
-        if regexp_user.search(line) is not None:
-          cnt_user += 1
           line = line.replace("SPACEUSER", user)
         print line,
-  return (cnt, cnt_user)
+  return cnt
 
 if __name__ == "__main__":
   parser = argparse.ArgumentParser()
