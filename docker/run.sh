5c083f020b24026535f742fda11d61972c921359
Simon Li <spli@dundee.ac.uk>
2019-03-07 10:11:55 +0000

    Workaround Docker socket permissions
    /var/run/docker.sock is mounted from the host into the docker node. Since Jenkins runs as a non-privileged user the Jenkins user must be a member of the docker group, and the docker group ID inside the node must match that of the host. Therefore run.sh is changes the GID of the docker group inside the node

--- docker/run.sh
+++ docker/run.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/tmp/jenkins-slave.sh
+set -eu
+set -x
+
+# Adjust docker permissions
+# https://github.com/jenkinsci/docker/issues/263#issuecomment-217955379
+sudo groupmod -g $(stat -c %g /var/run/docker.sock) docker
+sudo usermod -aG docker omero
+
+exec sudo -iu omero env SLAVE_PARAMS="$SLAVE_PARAMS" SLAVE_EXECUTORS="$SLAVE_EXECUTORS" SLAVE_NAME="$SLAVE_NAME" JENKINS_MASTER="$JENKINS_MASTER" /tmp/jenkins-slave.sh
