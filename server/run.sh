6ea59155020261a02d23aa3c865a2fe903d64709
Jean-Marie Burel <j.burel@dundee.ac.uk>
2022-01-18 13:09:04 +0000

    prepare installation

--- server/run.sh
+++ server/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function shut_down() {
-    /home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin stop
+    /home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin stop
 }
 
 /tmp/jenkins-slave.sh &
-/home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin start
-/home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin diagnostics
+/home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin start
+/home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin diagnostics
 
 trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
