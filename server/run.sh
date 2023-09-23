adf93c1d912aa53ba83669b9635529dd0d4eb141
Jean-Marie Burel <j.burel@dundee.ac.uk>
2023-09-23 19:16:34 +0100

    remove omero-server and omero-web user. Adding complexity

--- server/run.sh
+++ server/run.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 function shut_down() {
-    /home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin stop
+    /home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin stop
 }
 
 /tmp/jenkins-slave.sh &
-/home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin start
-/home/omero-server/workspace/OMERO-server/OMERO.server/bin/omero admin diagnostics
+/home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin start
+/home/omero/workspace/OMERO-server/OMERO.server/bin/omero admin diagnostics
 
 trap "shut_down" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 
